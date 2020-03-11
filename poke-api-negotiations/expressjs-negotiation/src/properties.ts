484c03a44a98557fde74de48270f37cda6a0197f
Reinaldo Borges Salla <reinaldo.salla@pampa.compasso>
2020-03-11 16:05:05 -0300

    First commit

---
+++ poke-api-negotiations/expressjs-negotiation/src/properties.ts
@@ -0,0 +1,6 @@
+export const hostname: string = "localhost";
+//export const port: number = 3000;
+export const port: number = 9001; 
+export const dbName: string = "test";
+//export const dbUrl: string = `mongodb://${hostname}:${dbName}`;
+export const dbUrl: string = "mongodb://localhost:27017/test";
